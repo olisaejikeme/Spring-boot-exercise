8b3c2bcc5fea3678e781a5f4f8f926417a0e93f8
Olisa Ejikeme <olisaejikeme@gmail.com>
2021-04-09 11:59:58 +0100

    Added security

---
+++ src/main/java/com/example/demo/web/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.demo.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/page/list-students").setViewName("list");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
